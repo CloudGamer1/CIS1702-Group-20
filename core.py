f72de84863ef8bf9a8895cac687b40a9a0e2351f
Klaud <87148573+CloudGamer1@users.noreply.github.com>
2025-12-28 21:19:58 +0000

    pick up treasure when you walk over it

--- core.py
+++ core.py
@@ -2,7 +2,6 @@ from MapLogic import *
 from PlayerLogic import *
 
 MapSave=LoadMap("Game-Map.csv")
-print(MapSave[1][2])
 TreasureData=LoadTresureData()
 
 PlayerLocation=[1,1]
@@ -23,4 +22,10 @@ while True:
     elif command == "d":
         PlayerLocation=MoveRight(PlayerLocation,MapSave)
 
+    if MapSave[PlayerLocation[0]][PlayerLocation[1]][:8].lower() == "treasure":
+        print("You found a treasure!")
+        treasure=GiveTreasure(TreasureData,MapSave[PlayerLocation[0]][PlayerLocation[1]])
+        print(f"You found treasure: {treasure}")
+        #when player inventory gets added add the treasure to inventory here
+    
     print(DisplayMap(PlayerLocation, MapSave))
