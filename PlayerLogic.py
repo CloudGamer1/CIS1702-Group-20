98ba57abfc069edcc3da067eb973270411fe83bc
Aidenleigh0982007 <aidenleigh63@gmail.com>
2026-01-06 02:08:41 +0000

    Enhancement of comments in PlayerLogic
    Added comments to clarify player movement and collision logic.

--- PlayerLogic.py
+++ PlayerLogic.py
@@ -1,5 +1,5 @@
 class Player():
-    def __init__(self):
+    def __init__(self): # Initiallises the player class with a starting position and health
         self.Player_Health = 100
         self.Player_Max_Health = 100
         self.PlayerLocation = [1,1]
@@ -10,11 +10,11 @@ class Player():
         self.PlayerLocation[0]-=1
 
         if map[self.PlayerLocation[0]][self.PlayerLocation[1]] == "wall":
-            self.CollideWall("Up")
+            self.CollideWall("Up") # Displays a message when colliding and places back to where the character was already
         elif map[self.PlayerLocation[0]][self.PlayerLocation[1]] == "Npc1":
-            self.CollideNpc("Npc1","Up")
+            self.CollideNpc("Npc1","Up") # Displays a message when colliding with an npc and places back to where the character was already
         elif map[self.PlayerLocation[0]][self.PlayerLocation[1]] == "Npc2":
-            self.CollideNpc("Npc2","Up")
+            self.CollideNpc("Npc2","Up") 
         return self.PlayerLocation
 
     def MoveDown(self,map):
@@ -23,7 +23,7 @@ class Player():
         self.PlayerLocation[0]+=1
 
         if map[self.PlayerLocation[0]][self.PlayerLocation[1]] == "wall":
-            self.CollideWall("Down")
+            self.CollideWall("Down") 
         elif map[self.PlayerLocation[0]][self.PlayerLocation[1]] == "Npc1":
             self.CollideNpc("Npc1","Down")
         elif map[self.PlayerLocation[0]][self.PlayerLocation[1]] == "Npc2":
@@ -58,44 +58,48 @@ class Player():
     
     def CollideNpc(self,Npc,Movement_Angle):
         if Npc == "Npc1":
-            if Movement_Angle == "Up":
-                print("Heres a Nutritious Meal!")
-                self.Player_Health += 10
-                self.Collide(Movement_Angle)
+            if Movement_Angle == "Up": # Direction being moved
+                print("Heres a Nutritious Meal!") # Message to show what happens when touching npc
+                self.Player_Health += 10 # Heals the player 10 health
+                if self.Player_Health > self.Player_Max_Health: # Ensures the player doesnt go above its max health
+                    self.Player_Health = self.Player_Max_Health
+                self.Collide(Movement_Angle) # Collision detection
             if Movement_Angle == "Left":
                 print("Heres a Nutritious Meal!")
                 self.Player_Health += 10
+                if self.Player_Health > self.Player_Max_Health:
+                    self.Player_Health = self.Player_Max_Health
                 self.Collide(Movement_Angle)
             if Movement_Angle == "Down":
                 print("Heres a Nutritious Meal!")
                 self.Player_Health += 10
+                if self.Player_Health > self.Player_Max_Health:
+                    self.Player_Health = self.Player_Max_Health
                 self.Collide(Movement_Angle)
             if Movement_Angle == "Right":
                 print("Heres a Nutritious Meal!")
                 self.Player_Health += 10
+                if self.Player_Health > self.Player_Max_Health:
+                    self.Player_Health = self.Player_Max_Health
                 self.Collide(Movement_Angle)
         if Npc == "Npc2":
             if Movement_Angle == "Up":
                 print("You were attacked by a Goblin!")
-                self.Player_Health += 10
                 self.Collide(Movement_Angle)
         if Movement_Angle == "Left":
                 print("You were attacked by a Goblin!")
-                self.Player_Health += 10
                 self.Collide(Movement_Angle)
         if Movement_Angle == "Down":
                 print("You were attacked by a Goblin!")
-                self.Player_Health += 10
                 self.Collide(Movement_Angle)
         if Movement_Angle == "Right":
                 print("You were attacked by a Goblin!")
-                self.Player_Health += 10
                 self.Collide(Movement_Angle)
 
     def CollideWall(self,Movement_Angle):
         if Movement_Angle == "Up":
-            print("You hit a wall! Can't move up.")
-            self.Collide(Movement_Angle)
+            print("You hit a wall! Can't move up.") # Displays a message showing why the player cant move
+            self.Collide(Movement_Angle) # Collision detection
         if Movement_Angle == "Left":
             print("You hit a wall! Can't move left.")
             self.Collide(Movement_Angle)
@@ -107,11 +111,11 @@ class Player():
             self.Collide(Movement_Angle)
         
     def Collide(self,Movement_Angle):
-        if Movement_Angle == "Up":
-            self.PlayerLocation[0]+=1
+        if Movement_Angle == "Up": 
+            self.PlayerLocation[0]+=1 # Moves player back down 1 place in the y axis
             return self.PlayerLocation
         if Movement_Angle == "Left":
-            self.PlayerLocation[1]+=1
+            self.PlayerLocation[1]+=1 # Moves player back down 1 place in the x axis
             return self.PlayerLocation
         if Movement_Angle == "Down":
             self.PlayerLocation[0]-=1
